f5f521092d42c73d1d757c7cee20ccb8f9c50ce8
Jan Jonas Willumeit <tsubajashi@gmail.com>
2019-07-29 06:45:01 +0200

    Update SSimSuperRes.glsl

--- Shaders/SSimSuperRes.glsl
+++ Shaders/SSimSuperRes.glsl
@@ -28,7 +28,7 @@
 #define offset      vec2(0,0)
 
 #define MN(B,C,x)   (x <= 1.0 ? ((2.-1.5*B-C)*x + (-3.+2.*B+C))*x*x + (1.-B/3.) : (((-B/6.-C)*x + (B+5.*C))*x + (-2.*B-8.*C))*x+((4./3.)*B+4.*C))
-#define Kernel(x)   MN(0.67962275898295921, 0.1601886205085204, abs(x))
+#define Kernel(x)   MN(0.2620145123990142, 0.3689927438004929, abs(x))
 #define taps        2.0
 
 #define Kb 0.0722
@@ -76,7 +76,7 @@ vec4 hook() {
 #define offset      vec2(0,0)
 
 #define MN(B,C,x)   (x <= 1.0 ? ((2.-1.5*B-C)*x + (-3.+2.*B+C))*x*x + (1.-B/3.) : (((-B/6.-C)*x + (B+5.*C))*x + (-2.*B-8.*C))*x+((4./3.)*B+4.*C))
-#define Kernel(x)   MN(0.67962275898295921, 0.1601886205085204, abs(x))
+#define Kernel(x)   MN(0.2620145123990142, 0.3689927438004929, abs(x))
 #define taps        2.0
 
 #define Kb 0.0722
