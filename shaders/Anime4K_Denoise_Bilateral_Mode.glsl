2dc8a75ffb1857ce2037576ed40fca6567df93b7
Jan Jonas Willumeit <tsubajashi@gmail.com>
2020-06-21 12:30:30 +0200

    Overhaul
    Mixed Upscaler is SSSR now.
    If you watch only a specific content type, you can choose the "ultra shader". they are extremely demanding though, unlike SSSR.

--- shaders/Anime4K_Denoise_Bilateral_Mode.glsl
+++ shaders/Anime4K_Denoise_Bilateral_Mode.glsl
@@ -23,13 +23,13 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-//!DESC Anime4K-v3.1-Denoise-Bilateral-Mean
+//!DESC Anime4K-v3.1-Denoise-Bilateral-Mode
 //!HOOK NATIVE
 //!BIND HOOKED
 
 #define INTENSITY_SIGMA 0.1 //Intensity window size, higher is stronger denoise, must be a positive real number
 #define SPATIAL_SIGMA 1.0 //Spatial window size, higher is stronger denoise, must be a positive real number.
-
+#define HISTOGRAM_REGULARIZATION 0.2 //Histogram regularization window size, higher values approximate a bilateral "closest-to-mean" filter.
 
 #define INTENSITY_POWER_CURVE 1.0 //Intensity window power curve. Setting it to 0 will make the intensity window treat all intensities equally, while increasing it will make the window narrower in darker intensities and wider in brighter intensities.
 
@@ -44,9 +44,24 @@ float gaussian(float x, float s, float m) {
 	return exp(-0.5 * scaled * scaled);
 }
 
+vec4 getMode(vec4 v[KERNELLEN], float w[KERNELLEN]) {
+	vec4 maxv = vec4(0);
+	float maxw = 0;
+	
+	for (int i=0; i<KERNELLEN; i++) {
+		if (w[i] >= maxw) {
+			maxw = w[i];
+			maxv = v[i];
+		}
+	}
+	
+	return maxv;
+}
+
 vec4 hook() {
-	vec4 sum = vec4(0);
-	float n = 0;
+	vec4 histogram_v[KERNELLEN];
+	float histogram_w[KERNELLEN];
+	float histogram_wn[KERNELLEN];
 	
 	float vc = HOOKED_tex(HOOKED_pos).x;
 	
@@ -55,11 +70,19 @@ vec4 hook() {
 	
 	for (int i=0; i<KERNELLEN; i++) {
 		vec2 ipos = GETOFFSET(i);
-		vec4 v = HOOKED_texOff(ipos);
-		float d = gaussian(v.x, is, vc) * gaussian(length(ipos), ss, 0);
-		sum += d * v;
-		n += d;
+		histogram_v[i] = HOOKED_texOff(ipos);
+		histogram_w[i] = gaussian(histogram_v[i].x, is, vc) * gaussian(length(ipos), ss, 0);
+		histogram_wn[i] = 0;
 	}
 	
-	return sum / n;
+	for (int i=0; i<KERNELLEN; i++) {
+		histogram_wn[i] += gaussian(0, HISTOGRAM_REGULARIZATION, 0) * histogram_w[i];
+		for (int j=(i+1); j<KERNELLEN; j++) {
+			float d = gaussian(histogram_v[j].x, HISTOGRAM_REGULARIZATION, histogram_v[i].x);
+			histogram_wn[j] += d * histogram_w[i];
+			histogram_wn[i] += d * histogram_w[j];
+		}
+	}
+	
+	return getMode(histogram_v, histogram_wn);
 }
